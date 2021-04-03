ad97598e9255e347bb230bb22a07c3539f5365f9
astrojose <jossey1918@gmail.com>
2021-04-03 11:24:38 +0300

    disable navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,9 +9,9 @@ const Navbar = () => {
 
             <nav className="nav">
                 <ul className="nav-links">
-                    <li><a href="#">learn</a></li>
+                    {/* <li><a href="#">learn</a></li>
                     <li><a href="#">locale</a></li>
-                    <li><a href="#">login</a> </li>
+                    <li><a href="#">login</a> </li> */}
                 </ul>
             </nav>
         </div>
