1f89ba7ae457437efa21a7a886c5cd03c9f48059
astrojose <jossey1918@gmail.com>
2021-04-05 02:59:23 +0300

    minimal with swahili

--- src/App.js
+++ src/App.js
@@ -4,27 +4,11 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import './styles/App.css'
 import Layout from './components/layout'
 import TypingGame from './pages/TypingGame'
-import Home from './pages/Home'
 
 function App(props) {
   return (
       <Layout>
-        <Router>
-                {/* <Route path="/" component={MainGame} /> */}
-                <Switch>
-                <Route path='/'>
-                    <Route exact path='/' component={Home} />
-
-                    <Route
-                      path='/typing-test'
-                      exact
-                      render={(routerProps) => (
-                        <TypingGame {...routerProps}/>
-                      )}
-                    />
-                </Route>
-                </Switch>
-          </Router>
+	  <TypingGame />
       </Layout>
   )
 }
