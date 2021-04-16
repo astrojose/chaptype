d17ee87b289ee5cde3840b1b3a7de6a33e55c03b
astrojose <jossey1918@gmail.com>
2021-04-16 20:16:10 +0300

    fix: basename route repetitions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-   <Router basename={process.env.PUBLIC_URL}> <App /> </Router>
+   <Router> <App /> </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
